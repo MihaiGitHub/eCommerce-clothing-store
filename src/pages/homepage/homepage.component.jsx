8292f7db029901bc4df8eea3b18e4a47a5528328
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-01-11 21:16:58 -0700

    Menu item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,12 +6,6 @@ const HomePage = () => (
     <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
-          <h1 className="title">HATS</h1>
-          <span className="subtitle"> SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
           <h1 className="title">JACKETS</h1>
           <span className="subtitle"> SHOP NOW</span>
         </div>
